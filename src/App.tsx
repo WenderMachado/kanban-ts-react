f15137fc5d6de3e99cdd1d4d2263ff1e2226c380
Wender Machado <devwendermachado@gmail.com>
2026-05-05 11:03:04 -0300

    feat: adicionar entidade ts para definição de moldes do objeto task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Box, Heading } from "@radix-ui/themes"
+import { PlusIcon } from "@radix-ui/react-icons"
+import { Box, Button, Flex, Heading } from "@radix-ui/themes"
 
 function App() {
 
 
   return (
-    <Box>
-        <Heading>React Kanban</Heading>
+    <Box maxWidth= "80-rem" mx="auto">
+      <Box height="4rem">
+        <Flex align= "center" gap="4" height="100%">
+          <Heading as="h1" size="8" weight="light">React Kanban</Heading>
+          <Button> 
+            <PlusIcon/>Nova Tarefa
+          </Button>
+        </Flex>
+      </Box>
+
+      <Box>
+        <Heading as="h2" >Quadro de Tarefas</Heading>
+      </Box>
+
     </Box>
   )
 }
---
+++ src/entities/task.ts
@@ -0,0 +1,9 @@
+export type TaskStatus = "todo" | "doing" | "done"
+export type TaskPriority = "low" | "medium" | "high"
+export interface Task{
+  id:number
+  title:string
+  description:string
+  status: TaskStatus
+  priorty: TaskPriority
+}
